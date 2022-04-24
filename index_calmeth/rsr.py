c3f2c006b13c0fd7195563195445e624245a41b8
skahanium <61221285+skahanium@users.noreply.github.com>
2022-04-24 10:28:13 +0800

    2022.4.24

--- index_calmeth/rsr.py
+++ index_calmeth/rsr.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import bisect
 from typing import Union
 
 
@@ -16,67 +15,60 @@ class Rsr:
         """
         初始化：得到可用数据矩阵及其长宽数据
         """
-        self.__df = dataframe.copy()
+        self.__df = np.array(dataframe)
         self.__m, self.__n = self.__df.shape
 
-    def score_matrix1(self, bv_list: list[num]) -> pd.DataFrame | None:
+    def score_matrix1(self, bv_list: list[num]) -> np.ndarray | None:
         """整次秩和比法计算得分，bv_list是由各指标正向最佳贡献值构成的列表
 
         Args:
             bv_list (list[num]): 正向最佳值列表
 
         Returns:
-            pd.DataFrame | None: 若参数无误，返回转化后的数据框，否则返回None
+            np.ndarray | None: 若参数无误，返回转化后的数据组，否则返回None
         """
         if len(bv_list) != self.__n:
             print("重新考虑最佳贡献值列表元素数量")
         else:
             # 计算距离矩阵
-            bv_matrix = pd.DataFrame(
-                np.tile(np.array(bv_list).T, (self.__m, 1)), columns=self.__df.columns)
-            dist_matrix = (self.__df - bv_matrix).abs()
+            bv_matrix = np.tile(np.array(bv_list), (self.__m, 1))
+            dist_matrix: np.ndarray = np.absolute(self.__df - bv_matrix)
             # 计算打分矩阵
-            rsr_matrix = pd.DataFrame(
-                np.empty((self.__m, self.__n)), columns=self.__df.columns)  # 创建与距离矩阵形状相同的空矩阵
+            rsr_matrix = np.empty((self.__m, self.__n))  # 创建与距离矩阵形状相同的空矩阵
             for q in range(self.__n):
-                compare_list = np.sort(dist_matrix.iloc[:, q])
-                for p in range(self.__m):
-                    rsr_matrix.iloc[p, q] = bisect.bisect_left(
-                        compare_list, dist_matrix.iloc[p, q])  # type: ignore
+                compare_list: np.ndarray = np.sort(dist_matrix[:, q])
+                rsr_matrix[:, q] = np.searchsorted(
+                    compare_list, dist_matrix[:, q])
             return rsr_matrix / self.__m * 100
 
-    def score_matrix2(self, bv_list: list[num]) -> pd.DataFrame | None:
+    def score_matrix2(self, bv_list: list[num]) -> np.ndarray | None:
         """非整次秩和比法计算得分，bv_list是由各指标正向最佳贡献值构成的列表
 
         Args:
             bv_list (list[num]): 正向最佳值列表
 
         Returns:
-            pd.DataFrame | None: 若参数无误，返回转化后的数据框，否则返回None
+            np.ndarray | None: 若参数无误，返回转化后的数据组，否则返回None
         """
         if len(bv_list) != self.__n:
             print("重新考虑最佳贡献值列表元素数量")
         else:
             # 计算距离矩阵
-            bv_matrix = pd.DataFrame(
-                np.tile(np.array(bv_list).T, (self.__m, 1)), columns=self.__df.columns)
-            dist_matrix = (self.__df - bv_matrix).abs()
+            bv_matrix = np.tile(np.array(bv_list), (self.__m, 1))
+            dist_matrix: np.ndarray = np.absolute(self.__df - bv_matrix)
             # 计算打分矩阵
-            rsr_matrix = pd.DataFrame(
-                np.empty((self.__m, self.__n)), columns=self.__df.columns)  # 创建与距离矩阵形状相同的空矩阵
+            rsr_matrix: np.ndarray = np.empty(
+                (self.__m, self.__n))  # 创建与距离矩阵形状相同的空矩阵
             for q in range(self.__n):
-                max_v = dist_matrix.iloc[:, q].max()
-                min_v = dist_matrix.iloc[:, q].min()
-                for p in range(self.__m):
-                    rsr_matrix.iloc[p, q] = 1 + ((self.__m - 1) * (
-                        dist_matrix.iloc[p, q] - min_v) / (max_v - min_v))
+                max_v = dist_matrix[:, q].max()
+                min_v = dist_matrix[:, q].min()
+                rsr_matrix[:, q] = 1 + ((self.__m - 1) *
+                                        (dist_matrix[:, q] - min_v) / (max_v - min_v))
             # 得分标准化
-            score_matrix = pd.DataFrame(
-                np.empty((self.__m, self.__n)), columns=self.__df.columns)
+            score_matrix = np.empty((self.__m, self.__n))
             for q in range(self.__n):
-                max_v = rsr_matrix.iloc[:, q].max()
-                min_v = rsr_matrix.iloc[:, q].min()
-                for p in range(self.__m):
-                    score_matrix.iloc[p, q] = rsr_matrix.iloc[p,
-                                                              q] / (max_v - min_v) * 100
+                max_v = rsr_matrix[:, q].max()
+                min_v = rsr_matrix[:, q].min()
+                score_matrix[:, q] = rsr_matrix[:, q] / \
+                    (max_v - min_v) * 100
             return score_matrix
