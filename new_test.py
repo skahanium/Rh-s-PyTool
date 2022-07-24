0ce583bcb6611d4e239e76f93a89a37b35236f86
skahanium <61221285+skahanium@users.noreply.github.com>
2022-07-24 15:21:37 +0800

    换用polars前的版本

--- new_test.py
+++ new_test.py
@@ -2,14 +2,12 @@ import os
 from math import radians, cos, sin, asin, sqrt
 from jieba import lcut
 import jieba
-import pandas as pd
+import polars as pl
 import index_calmeth
 
 csv_dir = f"{os.path.dirname(index_calmeth.__file__)[:-13]}cp_lookup/attachment/adcodes.csv"
-data = pd.read_csv(csv_dir)
-data["adcode"] = data["adcode"].astype(str)
-data.sort_values("adcode", inplace=True)
-
+data = pl.read_csv(csv_dir)
+data["adcode"] = data["adcode"].apply(str)
 
 new_words = ['山南市', '林芝市', '昌都市', '普洱市', '海东市',
              '陇南市', '巴彦淖尔市', '襄阳市', '三沙市', '乌兰察布市']
@@ -87,63 +85,61 @@ NOTPROVINCE: list[str] = CITYCODE + COUNTYCODE
 NOTCOUNTY: list[str] = PROVINCECODE + CITYCODE
 
 
-def _level_choose(level: str | None) -> pd.DataFrame:
+def _level_choose(level: str | None) -> pl.DataFrame:
     options = ["province", "city", "county",
                "not province", "not county", None]
     assert level in options, "错误的等级选项！！！"
     match level:
         case "province":
-            return data.loc[data["adcode"].isin(PROVINCECODE)]
+            return data[data["adcode"].is_in(PROVINCECODE)]
         case "city":
-            return data.loc[data["adcode"].isin(CITYCODE)]
+            return data[data["adcode"].is_in(CITYCODE)]
         case "county":
-            return data.loc[data["adcode"].isin(COUNTYCODE)]
+            return data[data["adcode"].is_in(COUNTYCODE)]
         case "not province":
-            return data.loc[data["adcode"].isin(NOTPROVINCE)]
+            return data[data["adcode"].is_in(NOTPROVINCE)]
         case "not county":
-            return data.loc[data["adcode"].isin(NOTCOUNTY)]
+            return data[data["adcode"].is_in(NOTCOUNTY)]
         case _:
             return data
 
 
-def _num_one(info: list[str], level_data: pd.DataFrame):
+def _num_one(info: list[str], level_data: pl.DataFrame):
     addr = info[0]
-    if len(level_data.loc[level_data["name"].str.match(addr)]) != 1:
+    if len(level_data[level_data["name"].str.contains(addr)]) != 1:
         raise AreaNameError("地名重复或有误！！！")
     else:
-        return level_data.loc[level_data["name"].str.match(addr)]
+        return level_data[level_data["name"].str.contains(addr)]
 
 
-def _num_two(info: list[str], level_data: pd.DataFrame):
+def _num_two(info: list[str], level_data: pl.DataFrame):
     father, addr = info
     if father == addr:
         addr_name = SAMENAMES[addr][1]
-        return data.loc[data["name"] == addr_name]
+        return data[data["name"] == addr_name]
     else:
         fdata = _level_choose("not county")
-        fcode = fdata.loc[fdata["name"].str.match(
-            father)]["adcode"].to_list()[0]
-        addr_codes = level_data.loc[level_data["name"].str.match(
-            addr)]["adcode"].to_list()
+        fcode = fdata[fdata["name"].str.contains(father)][0, "adcode"]
+        addr_codes = level_data[level_data["name"].str.contains(
+            addr)]["adcode"]
         for code in addr_codes:
             if (code[:4] == fcode[:4]) | (code[:2] == fcode[:2]):
-                return level_data.loc[level_data["adcode"] == code]
+                return level_data[level_data["adcode"] == code]
 
 
-def _num_three(info: list[str], level_data: pd.DataFrame):
+def _num_three(info: list[str], level_data: pl.DataFrame):
     gdfather, father, addr = info
     gddata = _level_choose("province")
     fdata = _level_choose("city")
     ldata = _level_choose("county")
-    if len(level_data.loc[level_data["name"].str.match(addr)]) == 1:
-        return level_data.loc[level_data["name"].str.match(addr)]
-    gdcode = gddata.loc[gddata["name"].str.match(
-        gdfather)]["adcode"].to_list()[0]
-    fdcode = fdata.loc[fdata["name"].str.match(father)]["adcode"].to_list()[0]
-    addr_codes = ldata.loc[ldata["name"].str.match(addr)]["adcode"]
+    if len(level_data[level_data["name"].str.contains(addr)]) == 1:
+        return level_data[level_data["name"].str.contains(addr)]
+    gdcode = gddata[gddata["name"].str.contains(gdfather)][0, "adcode"]
+    fdcode = fdata[fdata["name"].str.contains(father)][0, "adcode"]
+    addr_codes = ldata[ldata["name"].str.contains(addr)]["adcode"]
     for code in addr_codes:
         if code[:2] == fdcode[:2] == gdcode[:2]:
-            return level_data.loc[level_data["adcode"] == code]
+            return level_data[level_data["adcode"] == code]
 
 
 class Addr():
@@ -151,7 +147,7 @@ class Addr():
 
     def __init__(self, name: str, adcode: int | None = None, level: str | None = None):
         if adcode:
-            self.addr = data.loc[data["adcode"] == adcode]
+            self.addr = data[data["adcode"] == adcode]
         else:
             obj_area_data = _level_choose(level) if level else data
             info = lcut(name)
@@ -165,17 +161,18 @@ class Addr():
                     self.addr = _num_three(info, obj_area_data)
 
     def _belongs_to(self) -> str | None:
-        assert isinstance(self.addr, pd.DataFrame)
-        code = self.addr["adcode"].to_list()[0]
+        assert isinstance(self.addr, pl.DataFrame)
+        code = self.addr[0, "adcode"]
         fcode = code[:2] + "0" * \
-            10 if str.endswith(code, "0"*8) else (code[:4] + "0"*8)
-        findex = data.loc[data["adcode"] == fcode].index.to_list()[0]
-        return data.at[findex, "name"]
+            10 if str.endswith(
+                code, "0"*8) else (code[:4] + "0"*8)  # type: ignore
+        fdata = data[data["adcode"] == fcode]
+        return fdata[0, "name"]  # type: ignore
 
     def _coordinate(self) -> tuple[float, float]:
-        assert isinstance(self.addr, pd.DataFrame)
-        area_index = self.addr.index.to_list()[0]
-        return data.at[area_index, "latitude"], data.at[area_index, "longitude"]
+        lat = self.addr[0, "latitude"]  # type: ignore
+        lon = self.addr[0, "longitude"]  # type: ignore
+        return lat, lon  # type: ignore
 
 
 def lookup(name: str, level: str | None = None) -> str | None:
@@ -192,9 +189,7 @@ def lookup(name: str, level: str | None = None) -> str | None:
         obj_area = Addr(new_name, level="province").addr
     else:
         obj_area = Addr(name, level=level).addr
-    assert obj_area is not None
-    area_index = obj_area.index.to_list()[0]
-    return data.at[area_index, "name"]
+    return obj_area[0, "name"]  # type: ignore
 
 
 def belongs_to(name: str, level: str | None = None) -> str | None:
@@ -270,3 +265,12 @@ def dist(city1: str, city2: str, level1: str | None = None, level2: str | None =
     city1_lat, city1_lon = coordinate(city1, level=level1)
     city2_lat, city2_lon = coordinate(city2, level=level2)
     return haversine(city1_lat, city1_lon, city2_lat, city2_lon)
+
+
+if __name__ == "__main__":
+    print(Addr("四川广元昭化").addr[0, "adcode"])  # type: ignore
+    print(Addr("青海海南")._belongs_to())
+    print(Addr("青海海南")._coordinate())
+    print(lookup("辽宁铁岭"))
+    print(belongs_to("玉树", level="city"))
+    print(coordinate("常德市", level="city"))
--- src/cp_lookup/__init__.py
+++ src/cp_lookup/__init__.py
@@ -1,4 +1,4 @@
-from .main import Addr, belongs_to, lookup, dist, coordinate, haversine
+from .main import Addr, belongs_to, lookup, dist, coordinate, haversine, PROVINCECODE, CITYCODE, COUNTYCODE
 
 if __name__ != "__main__":
     HOMETOWN = lookup("广元昭化")
